eb0da17dfa642d177cd55eae7d90ee8dd0e16402
Tom Straub <tstraub@hashicorp.com>
2020-03-26 13:36:39 -0500

    Update main.tf

--- app-vm-dev/main.tf
+++ app-vm-dev/main.tf
@@ -37,8 +37,8 @@ resource "vsphere_virtual_machine" "vm" {
   name             = local.virtual_machine_name
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 2
-  memory           = 1024
+  num_cpus         = 1
+  memory           = 512
   guest_id         = "other3xLinux64Guest"
 
   network_interface {
